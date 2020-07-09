d68f8507b2d36f356ca64e449a57d1975f94bc74
tjc <tjcux@qq.com>
2020-07-09 13:57:43 +0800

    增加mapper扫描

--- stdls-backend-web/src/main/java/com/daidai/stdlsbackend/StdlsBackendApplication.java
+++ stdls-backend-web/src/main/java/com/daidai/stdlsbackend/StdlsBackendApplication.java
@@ -1,9 +1,11 @@
 package com.daidai.stdlsbackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.daidai.stdlsbackend.dao*")
 public class StdlsBackendApplication {
 
 	public static void main(String[] args) {
